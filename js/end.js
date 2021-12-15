ffb936a34b408e9a74f9c532d7e60a4c5834094b
Teracle <mnosov622@gmail.com>
2021-12-16 02:55:48 +0700

    Game is ready but requires updates

---
+++ js/end.js
@@ -0,0 +1,37 @@
+const username=document.getElementById('username');
+const saveScoreBtn=document.getElementById('saveScoreBtn');
+
+const mostRecentScore=localStorage.getItem('mostRecentScore');
+const finalScore=document.getElementById('finalScore');
+
+const highScores=JSON.parse(localStorage.getItem("highScores")) || [];
+
+const max_high_scores=5;
+console.log(JSON.parse(localStorage.getItem("highScores")));
+finalScore.innerText=mostRecentScore;
+username.addEventListener('keyup',()=>{
+    //console.log(username.value);
+     
+    saveScoreBtn.disabled=!username.value;
+});
+
+saveHighScore=(e,res)=>{
+
+    console.log("Clicked the save button");
+    e.preventDefault()
+    const score={
+        score:mostRecentScore,
+        name:username.value
+    };
+    highScores.push(score);
+    
+    highScores.sort((a,b)=>{
+        return b.score-a.score;
+    });
+
+    highScores.splice(5);
+
+    localStorage.setItem("highScores",JSON.stringify(highScores));
+    window.location.assign('/');
+
+};
---
+++ server.js
@@ -0,0 +1,43 @@
+const express=require("express");
+const { Http2ServerRequest } = require("http2");
+const app=express();
+const exphbs=require("express-handlebars");
+var HTTP_PORT=process.env.HTTP_PORT || 8080;
+
+
+app.engine('.html',exphbs.engine({
+    extname:'.html',
+    defaultLayout:''
+}));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("static"));
+app.use(express.static("js"));
+app.set('view engine','.html')
+
+app.get("/",(req,res)=>{
+   res.render("index");
+});
+app.get("/game",(req,res)=>{
+    res.render("game",{
+        title:"Play"
+    });
+});
+ 
+app.get("/highscores",(req,res)=>{
+    res.render("highscores",{
+        title:"Highscores"
+    });
+});
+
+app.get("/end",(req,res)=>{
+    res.render("end",{
+        title:"End"
+    });
+});
+ 
+
+function onHttpStart(){
+    console.log("Web server is running on port: "+HTTP_PORT);
+}
+
+app.listen(HTTP_PORT,onHttpStart);
