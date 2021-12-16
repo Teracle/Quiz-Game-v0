10dc4a1697de0b80fa71d92ef13d76e11ca4c34a
Teracle <mnosov622@gmail.com>
2021-12-16 23:33:26 +0700

    Music added2

--- js/game.js
+++ js/game.js
@@ -88,7 +88,7 @@ getNewQuestion=()=>{
     
     if(availableQuestions.length==0 && questionCounter>=max_questions){
     localStorage.setItem('mostRecentScore',score);
-        return window.location.assign('../views/end.html');
+        return window.location.assign('end.html');
     }
 
 questionCounter++;
